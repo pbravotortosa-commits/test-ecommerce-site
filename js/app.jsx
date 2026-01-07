3d5f11a036ca3760a7fcacde46e317a941ca568c
pbravotortosa-commits <pbravotortosa@gmail.com>
2026-01-07 15:40:24 +0100

    Update app.jsx

--- js/app.jsx
+++ js/app.jsx
@@ -53,7 +53,7 @@ function App() {
       return [...prev, { id: product.id, name: product.name, price: product.price, image: product.image, qty: 1 }];
     });
     analytics.track("Product Added to the cart", {
-      product: "Product X",
+      product: "Product " + product.id,
     });	
   }
 
